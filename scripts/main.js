c6ee5ab92038277a616396c20c5cb69856dda5da
Stephanie Mahnke <steph.mahnke@gmail.com>
2026-04-18 12:16:17 -0500

    Added main.js structure

--- scripts/main.js
+++ scripts/main.js
@@ -3,13 +3,27 @@ const mainContainer = document.querySelector("#container");
 
 const applicationHTML = `
 <header class="header">
+    <img src="https://cdn.creazilla.com/icons/3263938/self-improvement-icon-sm.png" alt="logo" class="logo">
     <h1>Daily Rebuild [SM]</h1>
-    <img src="https://cdn.creazilla.com/icons/3263938/self-improvement-icon-sm.png" alt="logo" class="logo"></img>
 </header>
+
     <form class="entryForm">
         <fieldset>
             <label for="entryDate">Date</label>
             <input type="date" name="entryDate" class="entryForm__date">
+        </fieldset>
+
+        <fieldset>
+            <label for="Mood">Mood for the day</label>
+            <input type="text" list="entry-options" id="entry-choice" name="entry-choice" placeholder="Select or type...">
+
+                <datalist id="entry-options">
+                    <option value="amazing">
+                    <option value="good">
+                    <option value="meh">
+                    <option value="bad">
+                    <option value="awful">
+                </datalist>
         </fieldset>
     </form>
 <article class="services">
@@ -21,7 +35,7 @@ const applicationHTML = `
         
 
     <article class="guests">
-        <h2>Current Guests</h2>
+        <h2>Completion Rates by Week</h2>
        
     </article>
 </section>
